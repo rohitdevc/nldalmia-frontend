c49a9ce3d16951363b61bfebdcdf88d622bc29f0
Rohit <rohitc129@gmail.com>
2026-02-11 13:21:33 +0530

    blocked netlify from crawlers

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
+const isNetlifyDomain = process.env.URL?.includes("netlify.app");
+
 const nextConfig: NextConfig = {
+	async headers() {
+	    if (!isNetlifyDomain) return [];
+	    
+	    return [
+	      {
+	        source: "/(.*)",
+	        headers: [
+	          {
+	            key: "X-Robots-Tag",
+	            value: "noindex, nofollow",
+	          },
+	        ],
+	      },
+	    ];
+	 }
 };
 
 export default nextConfig;
