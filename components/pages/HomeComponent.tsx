991cd7320fece96c3429ca486d9498bed4384eab
unknown <rohitc129@gmail.com>
2026-02-28 15:00:57 +0530

    life at nld page

--- components/pages/HomeComponent.tsx
+++ components/pages/HomeComponent.tsx
@@ -32,6 +32,7 @@ import "swiper/css/navigation";
 
 import nl2br from 'nl2br';
 import parser from 'html-react-parser';
+import InstagramFeed from "@/components/InstagramFeed";
 
 export default function HomeComponent() {
   const basePath = process.env.NEXT_PUBLIC_PATH;
@@ -368,47 +369,6 @@ export default function HomeComponent() {
       blog_thumbnail: 'blog-thumbnail.png'
     }
   ]
-
-  const instagram_feed = [
-    {
-      id: 1,
-      instagram_feed_thumbnail: 'ig-thumbnail.png',
-      instagram_feed_caption: 'Congratulations',
-      instagram_feed_type: 'IMAGE',
-      instagram_feed_link: 'https://www.instagram.com/nldalmiainstitute/'
-    },
-    {
-      id: 2,
-      instagram_feed_thumbnail: 'ig-thumbnail.png',
-      instagram_feed_caption: 'Congratulations',
-      instagram_feed_type: 'IMAGE',
-      instagram_feed_link: 'https://www.instagram.com/nldalmiainstitute/'
-    },
-    {
-      id: 3,
-      instagram_feed_thumbnail: 'ig-thumbnail.png',
-      instagram_feed_caption: 'Congratulations',
-      instagram_feed_type: 'IMAGE',
-      instagram_feed_link: 'https://www.instagram.com/nldalmiainstitute/'
-    },
-    {
-      id: 4,
-      instagram_feed_thumbnail: 'ig-thumbnail.png',
-      instagram_feed_caption: 'Congratulations',
-      instagram_feed_type: 'IMAGE',
-      instagram_feed_link: 'https://www.instagram.com/nldalmiainstitute/'
-    },
-    {
-      id: 5,
-      instagram_feed_thumbnail: 'ig-thumbnail.png',
-      instagram_feed_caption: 'Congratulations',
-      instagram_feed_type: 'IMAGE',
-      instagram_feed_link: 'https://www.instagram.com/nldalmiainstitute/',
-      instagram_feed_video: ''
-    }
-  ]
-
-  const instagram_allowed_types = ['IMAGE', 'VIDEO'];
 
   const [activeEventCategory, updateActiveEventCategory] = useState(1);
   const [activeEvent, updateActiveEvent] = useState(0);
@@ -1047,45 +1007,12 @@ export default function HomeComponent() {
         </div>
       )
       }
-      {
-        instagram_feed && instagram_feed.length > 0 && (
+      
         <div className="w-full flex flex-col gap-5 px-5 md:px-15 xl:px-30 py-10">
           <Intro introTitle="Inspiring Moments, Captured In Every Frame." introCaption="Because Every Post Tells A Story <br/> Worth Sharing." />
-          <div className="flex gap-3">
-            <span className="w-5 h-5 border border-[#800000] flex items-center cursor-pointer instagram_slider_prev">
-              <BsArrowLeftShort size={20} />
-            </span>
-            <span className="w-5 h-5 border border-[#800000] flex items-center cursor-pointer instagram_slider_next">
-              <BsArrowRightShort size={20} />
-            </span>
-          </div>
-          <Swiper className="w-full text-white" slidesPerView={1} spaceBetween={40} loop={true} modules={[Navigation]} navigation={{prevEl: '.instagram_slider_prev', nextEl: '.instagram_slider_next'}} breakpoints={{480: { slidesPerView: 2, spaceBetween: 20 },768: { slidesPerView: 3, spaceBetween: 15 }, 1024: { slidesPerView: 3, spaceBetween: 50 }, 1280: { slidesPerView: 4, spaceBetween: 70 } }}>
-          {
-            instagram_feed.map((instagram, key) => (
-              instagram_allowed_types.includes(instagram.instagram_feed_type) && (
-                <SwiperSlide key={key}>
-                  <div className="group w-full" key={key} title={instagram.instagram_feed_caption}>
-                    <Link href={instagram.instagram_feed_link} target="_blank">
-                    {
-                      instagram.instagram_feed_type === 'IMAGE' && (
-                        <Image src={`${basePath}images/home/${instagram.instagram_feed_thumbnail}`} alt={instagram.instagram_feed_caption} width={300} height={300} className="object-cover" />
-                      )
-                    }
-                    {
-                    instagram.instagram_feed_type === 'VIDEO' && (
-                        <video src={`${basePath}videos/home/${instagram.instagram_feed_video}`} className="object-cover w-full h-full" muted loop playsInline autoPlay />
-                      )
-                    }
-                    </Link>
-                  </div>
-              </SwiperSlide>
-              )
-            ))
-          }
-          </Swiper>
+          <InstagramFeed />
         </div>
-      )
-      }
+      
       <Footer />
       <YTVideoPopUp ref={videoPopupRef} />
     </main>
--- components/pages/LifeAtNLDComponent.tsx
+++ components/pages/LifeAtNLDComponent.tsx
@@ -24,6 +24,7 @@ import Footer from "@/components/Footer";
 import Banner from "@/components/Banner";
 import Intro from "@/components/Intro";
 import CenterIntro from "@/components/CenterIntro";
+import InstagramFeed from "@/components/InstagramFeed";
 
 import dayjs from 'dayjs';
 import utc from "dayjs/plugin/utc";
@@ -152,6 +153,35 @@ export default function LifeAtNLD() {
     }
   }
 
+  const holistic_learnings = [
+    {
+      id: 1,
+      holistic_learning_title: 'Academic Infrastructure'
+    },
+    {
+      id: 2,
+      holistic_learning_title: 'Student Wellness'
+    }
+  ]
+
+  const [activeHolisticLearningTab, updateActiveHolisticLearningTab] = useState(1);
+
+  const HolisticLearnings = useRef<HTMLDivElement>(null);
+
+  const updateActiveHolisticLearningTabFunc = (holistic_learning_tab_id: number): void => {
+    updateActiveHolisticLearningTab(holistic_learning_tab_id);
+
+    if(HolisticLearnings.current) {
+      const offset = 200;
+      const elementTop = HolisticLearnings.current.getBoundingClientRect().top + window.pageYOffset;
+
+      window.scrollTo({
+        top: elementTop - offset,
+        behavior: 'smooth'
+      })
+    }
+  }
+
   return (
     <>
     <Header />
@@ -164,7 +194,7 @@ export default function LifeAtNLD() {
       <div className="w-full px-5 lg:px-30 py-5 lg:py-10 flex flex-col gap-5">
         <CenterIntro introTitle="Events" introCaption="Campus Buzz: Events That Define Us" introDescription="From national sympoisums and speaker series to cultural tests and student-led workshops-there’s always something happening at NLDIMSR" />
         <div className="flex flex-col lg:flex-row gap-5 md:justify-between">
-          <ul className="flex flex-col justify-center lg:justify-start items-center lg:items-start lg:w-100 gap-5 text-burgundy">
+          <ul className="flex flex-col justify-center lg:justify-start items-center lg:items-start gap-5 text-burgundy">
             {
               event_timelines.map((event_timeline, key) => (
                 <li className={`cursor-pointer transition-all duration-300 ${activeEventTimeline === event_timeline.id ? 'text-xl font-medium' : ''}`} key={key} onClick={() => updateActiveEventTimelineFunc(event_timeline.id)}>
@@ -180,10 +210,10 @@ export default function LifeAtNLD() {
               event_timelines && event_timelines.length > 0 && event_timelines.map((event_timeline) => 
                 activeEventTimeline === event_timeline.id && event_timeline.events && event_timeline.events.length > 0 && (
                 <div className="w-full lg:w-[75%] flex flex-col gap-5 text-white" ref={eventsList} key={event_timeline.id}>
-                  <div className="grid grid-cols-2 gap-2 sm:gap-5 md:gap-10">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 sm:gap-5 md:gap-10 lg:gap-5">
                     {
                       event_timeline.events.map((event, key) => (
-                        <div className="group w-full h-50 md:!h-75 xl:!h-100 bg-cover bg-center bg-no-repeat relative overflow-hidden" style={{backgroundImage: `url(${basePath}images/home/events/${event.event_thumbnail})`}} onClick={handleEventClick(event.id)} key={key} title={event.event_name}>
+                        <div className="group w-full h-75 xl:h-100 bg-cover bg-center bg-no-repeat relative overflow-hidden" style={{backgroundImage: `url(${basePath}images/home/events/${event.event_thumbnail})`}} onClick={handleEventClick(event.id)} key={key} title={event.event_name}>
                           <div className="absolute top-0 left-0 inset-0 bg-black/30"></div>
                           
                           <div className="relative h-full w-full flex flex-col">
@@ -250,6 +280,151 @@ export default function LifeAtNLD() {
       </div>
       <div className="w-full px-5 lg:px-30 py-5 lg:py-10 flex flex-col gap-5">
         <CenterIntro introTitle="Student Clubs" introCaption="Where Passion Finds Purpose: Student Club" introDescription="Clubs at NLDIMSR go beyond extracurriculars-they foster leadership, collaboration and real world problem solving" />
+        <div className="flex gap-3">
+          <span className="w-5 h-5 border border-[#800000] flex items-center cursor-pointer student_club_slider_prev">
+            <BsArrowLeftShort size={20} />
+          </span>
+          <span className="w-5 h-5 border border-[#800000] flex items-center cursor-pointer student_club_slider_next">
+            <BsArrowRightShort size={20} />
+          </span>
+        </div>
+        <Swiper className="w-full border-[0.5px] border-[#DFC0C0]" slidesPerView={1} spaceBetween={0} modules={[Navigation]} navigation={{prevEl: '.student_club_slider_prev', nextEl: '.student_club_slider_next'}} >
+          {
+            [...Array(5)].map((_, i) => (
+            <SwiperSlide>
+              <div className="w-full flex flex-col md:flex-row">
+                <div className="md:w-sm">
+                  <Image src={`${basePath}images/life-at-nld/student-club.png`} alt="Student Club" width={300} height={300} className="object-cover w-full h-full" />
+                </div>
+                <div className="flex flex-col gap-5 p-5 md:p-10">
+                  <div className="flex flex-col gap-3">
+                    <h2 className="font-georgia text-xl">Finance Forum Club</h2>
+                    <p className="text-sm leading-loose text-[#4E4E4E]">Engages students in global finance through case studies, competitions, speaker sessions and simulations.</p>
+                  </div>
+                  <div className="flex flex-col gap-3 text-sm">
+                    <h2 className="font-georgia">Institutional Publications</h2>
+                    <ul className="text-[#4E4E4E] flex flex-col lg:flex-row gap-5 list-disc list-inside">
+                      <li>FinTalkNewsletter</li>
+                      <li>Industry Outlook Report</li>
+                    </ul>
+                  </div>
+                  <div className="flex flex-col gap-3 text-sm">
+                    <h2 className="font-georgia">Follow Us</h2>
+                    <Link href="" target="_blank">@Finance_Forum_NLD</Link>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))
+          }
+        </Swiper>
+      </div>
+      <div className="w-full px-5 lg:px-30 py-5 lg:py-10 flex flex-col lg:flex-row gap-5">
+        <div className="flex flex-col gap-5 lg:w-1/2">
+          <div className="w-full h-100 lg:h-125">
+            <Image src={`${basePath}images/life-at-nld/gallery-1.png`} width={900} height={900} alt="Image 1" className="object-cover w-full h-full" />
+          </div>
+          <div className="w-full h-100">
+            <Image src={`${basePath}images/life-at-nld/gallery-2.png`} width={900} height={550} alt="Image 2" className="object-cover w-full h-full" />
+          </div>
+          <div className="w-full h-100">
+            <Image src={`${basePath}images/life-at-nld/gallery-3.png`} width={900} height={550} alt="Image 2" className="object-cover w-full h-full" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 lg:w-1/2">
+          <div className="w-full h-100">
+            <Image src={`${basePath}images/life-at-nld/gallery-4.png`} width={900} height={550} alt="Image 1" className="object-cover w-full h-full" />
+          </div>
+          <div className="w-full h-100">
+            <Image src={`${basePath}images/life-at-nld/gallery-5.png`} width={900} height={550} alt="Image 2" className="object-cover w-full h-full" />
+          </div>
+          <div className="w-full lg:min-h-125 bg-[#800000] text-white flex flex-col gap-5 p-4 lg:p-7">
+            <h2 className="font-georgia text-2xl lg:text-3xl leading-relaxed">Shri Niranjanlal Dalmia - <br/> Founder, Visionary, Guiding Light</h2>
+            <p className="leading-relaxed lg:leading-loose text-sm">In 1995, the Late Shri Niranjanlal Dalmia laid the foundation of the N.L.Dalmia Educational Society with a vision far beyond brick and mortar-to create a world class institute that would mature ethical, globally competent business leaders. Deeply rooted in his values of integrity, knowledge and social progress. N.L.Dalmia Institute of Management Studies and Research began its academic journey in 1997. Today, it stands tall among India’s top B-schools- a living treatment to his legacy of purposeful education.</p>
+            <div className="flex flex-col gap-3 mt-auto">
+              <h3 className="text-xl">“Education is the noblest investment in a nation future.”</h3>
+              <p className="leading-loose text-sm">- Shri Niranjanlal Dalmia</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-5 px-5 md:px-15 xl:px-30 py-10">
+          <Intro introTitle="Instagram Feed" introCaption="@Nldimsr" introDescription="Follow @nldimsr for the latest stories, campus updates and student moments" />
+          <InstagramFeed />
+      </div>
+      <div className="w-full px-5 lg:px-30 py-5 lg:py-10 flex flex-col gap-5">
+        <CenterIntro introTitle="Queries" introCaption="State Of Art Infrastructure To Enable Holistic Learning" introDescription="Our campus is equipped with modern facilities to nurture innovation, wellbeing and academic excellence" />
+        <ul className="flex flex-col sm:flex-row gap-7 text-burgundy justify-center items-center">
+          {
+            holistic_learnings.map((holistic_learning_tab, key) => (
+              <li className={`group relative cursor-pointer transition-all duration-300 ${holistic_learning_tab.id === activeHolisticLearningTab ? 'text-burgundy text-2xl font-normal': 'text-[#4E4E4E] text-lg'}`} key={key} onClick={() => updateActiveHolisticLearningTabFunc(holistic_learning_tab.id)}>
+                <span>{holistic_learning_tab.holistic_learning_title}</span>
+                <span className={`absolute left-0 -bottom-1 bg-[#800000] h-[0.5px] w-full scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center ${holistic_learning_tab.id === activeHolisticLearningTab ? 'scale-x-100': ''} `}></span>
+              </li>
+            ))
+          }
+        </ul>
+        <div className="w-full" ref={HolisticLearnings}>
+        {
+            holistic_learnings.map((holistic_learning_tab, key) => (
+            <div className={`w-full ${holistic_learning_tab.id === activeHolisticLearningTab ? 'block' : 'hidden'}`} key={key}>
+              <div className="flex gap-3 mb-5">
+                <span className={`w-5 h-5 border border-[#800000] flex items-center cursor-pointer holistic_learning_${holistic_learning_tab.id}slider_prev`}>
+                  <BsArrowLeftShort size={20} />
+                </span>
+                <span className={`w-5 h-5 border border-[#800000] flex items-center cursor-pointer holistic_learning_${holistic_learning_tab.id}slider_next`}>
+                  <BsArrowRightShort size={20} />
+                </span>
+              </div>
+              <Swiper className="w-full" slidesPerView={1} spaceBetween={0} modules={[Navigation]} navigation={{prevEl: `.holistic_learning_${holistic_learning_tab.id}slider_prev`, nextEl: `.holistic_learning_${holistic_learning_tab.id}slider_next`}} >
+                {
+                  [...Array(5)].map((_, i) => (
+                  <SwiperSlide>
+                    <div className="w-full flex flex-col lg:flex-row">
+                      <div className="w-full lg:w-1/2 lg:order-2">
+                        <Image src={`${basePath}images/life-at-nld/holistic.png`} alt="Student Club" width={500} height={300} className="object-cover w-full h-full" />
+                      </div>
+                      <div className="flex flex-col gap-5 px-5 py-10 bg-[#FFCC33] w-full lg:w-1/2">
+                        <h2 className="font-georgia text-lg">Lecture Halls - {holistic_learning_tab.holistic_learning_title} - {i}</h2>
+                        <p className="text-sm leading-loose">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea.</p>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))
+                }
+              </Swiper>
+            </div>
+        ))}
+        </div>
+      </div>
+      <div className="w-full px-5 lg:px-30 py-5 lg:py-10 flex flex-col gap-5">
+        <CenterIntro introTitle="Queries" introCaption="Ideas In Print: Thought Leadership At Nldimsr" introDescription="Our community produces high quality journals, reports and newsletters-reflecting academic excellence and industry relevance" />
+        <div className="flex justify-center items-center">
+          <Link href="" className="bg-[#800000] text-white px-2 py-2">View All Institutional Publications</Link>
+        </div>
+        <div className={`w-full`}>
+          <div className="flex gap-3 mb-5">
+            <span className={`w-5 h-5 border border-[#800000] flex items-center cursor-pointer magazine_slider_prev`}>
+              <BsArrowLeftShort size={20} />
+            </span>
+            <span className={`w-5 h-5 border border-[#800000] flex items-center cursor-pointer magazine_slider_next`}>
+              <BsArrowRightShort size={20} />
+            </span>
+          </div>
+          <Swiper className="w-full" slidesPerView={2} spaceBetween={10} modules={[Navigation]} navigation={{prevEl: `.magazine_slider_prev`, nextEl: `.magazine_slider_next`}} breakpoints={{768: {slidesPerView: 2.5, spaceBetween: 50}, 1024: {slidesPerView: 3, spaceBetween: 50}}} >
+            {
+              [...Array(5)].map((_, i) => (
+              <SwiperSlide>
+                <div className="w-full flex flex-col lg:flex-row">
+                  <Link className="w-full" href="">
+                    <Image src={`${basePath}images/life-at-nld/goonj.png`} alt="Student Club" width={500} height={300} className="object-cover w-full h-full" />
+                  </Link>
+                </div>
+              </SwiperSlide>
+            ))
+            }
+          </Swiper>
+        </div>
       </div>
       <Footer />
     </main>
