42d0736ac1dbc9b5c76893802e21feb861865260
unknown <rohitc129@gmail.com>
2026-03-07 15:18:27 +0530

    faculty url slug fix

--- components/pages/FacultyDetailsComponent.tsx
+++ components/pages/FacultyDetailsComponent.tsx
@@ -33,7 +33,7 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 
-export default function FacultyDetails({event_url_slug = ""}) {
+export default function FacultyDetails({faculty_url_slug = ""}) {
   const basePath = process.env.NEXT_PUBLIC_PATH;
 
   const [openAccordian, toggleAccordian] = useState(1);
