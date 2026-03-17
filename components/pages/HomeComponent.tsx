cb0e55d32f9374667a49fbaa386aec703dd5884f
unknown <rohitc129@gmail.com>
2026-03-17 23:09:12 +0530

    integration wip

--- components/pages/HomeComponent.tsx
+++ components/pages/HomeComponent.tsx
@@ -551,7 +551,7 @@ export default function HomeComponent({banner, introduction, career_finder, care
               testimonials.map((testimonial, key) => (
                 <SwiperSlide className="group relative w-full md:!w-90 border border-[#800000] bg-white" title={testimonial.testimonial_name} key={key} onClick={handleTestimonialClick(key)}>
                   <div className="w-full h-full flex flex-col gap-10 px-5 py-5">
-                    <Image src={`${basePath}images/home/testimonials/${testimonial.testimonial_thumbnail}`} alt={testimonial.testimonial_name} width={200} height={200} className="rounded-full w-30 h-30" />
+                    <Image src={testimonial.testimonial_thumbnail} alt={testimonial.testimonial_name} width={200} height={200} className="rounded-full w-30 h-30" />
                     <h2 className="font-georgia text-xl lg:text-2xl">{testimonial.testimonial_name}</h2>
                     <div className="mt-auto flex flex-col gap-3 text-burgundy">
                       <span className="text-sm md:text-lg">{parser(nl2br(testimonial.testimonial_designation))}</span>
