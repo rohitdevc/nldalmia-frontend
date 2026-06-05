02bb5923368ea8eb4c059a383a400d8ac8aa4fd3
unknown <rohitc129@gmail.com>
2026-06-05 13:01:54 +0530

    reset header context

--- components/pages/HomeComponent.tsx
+++ components/pages/HomeComponent.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
