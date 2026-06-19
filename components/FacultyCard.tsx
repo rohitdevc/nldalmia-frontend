3aa8b967a9cd11c4a29bced3f020b42edac63be4
unknown <rohitc129@gmail.com>
2026-06-19 18:38:38 +0530

    view profile at bottom

--- components/FacultyCard.tsx
+++ components/FacultyCard.tsx
@@ -13,8 +13,8 @@ export function FacultyCard({faculty, keyIndex}: FacultyProps) {
     const basePath = process.env.NEXT_PUBLIC_PATH;
     
     return (
-        <div className="h-auto flex">
-            <div className="flex-1 h-full flex flex-col gap-4 xl:w-sm justify-center items-center text-center bg-white border-[0.5px] border-[#E0CDCD]" title={faculty.faculty_name} key={keyIndex}>
+        <div className="h-full flex">
+            <div className="flex-1 flex flex-col gap-4 xl:w-sm justify-center items-center text-center bg-white border-[0.5px] border-[#E0CDCD]" title={faculty.faculty_name} key={keyIndex}>
                 <div className="h-75">
                     {
                     faculty.faculty_thumbnail && (
@@ -24,7 +24,7 @@ export function FacultyCard({faculty, keyIndex}: FacultyProps) {
                 </div>
                 <h2 className="font-georgia text-xl">{faculty.faculty_name}</h2>
                 <span className="text-[#4E4E4E]">{faculty.faculty_designation}</span>
-                <Link className="w-full bg-[#800000] text-white py-1 flex justify-center items-center gap-2" href={`${basePath}faculty/${faculty.faculty_url_slug}`}>View Profile <MdArrowOutward size={20} /></Link>
+                <Link className="w-full bg-[#800000] text-white py-1 flex justify-center items-center gap-2 mt-auto" href={`${basePath}faculty/${faculty.faculty_url_slug}`}>View Profile <MdArrowOutward size={20} /></Link>
             </div>
         </div>
     )
