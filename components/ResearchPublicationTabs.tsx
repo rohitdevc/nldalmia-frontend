be32970978e55d5ab5ac7c990a08d5eec9b34569
unknown <rohitc129@gmail.com>
2026-06-22 18:41:58 +0530

    hash redirection

--- components/ResearchPublicationTabs.tsx
+++ components/ResearchPublicationTabs.tsx
@@ -27,11 +27,11 @@ export default function ResearchPublicationTabs({page_name}: ResearchPublication
     ]
 
     return (
-        <ul className="flex flex-wrap gap-5 lg:gap-10 justify-center items-center text-center px-5 sm:px-10 md:px-15 xl:px-20 2xl:px-30 py-10">
+        <ul className="flex flex-wrap gap-5 lg:gap-10 justify-center items-center text-center px-5 sm:px-10 md:px-15 xl:px-20 2xl:px-30 py-10" id="research-publication">
             {
                 tabs.map((tab, key) => (
                     <li key={key}>
-                        <Link href={`${basePath}faculty/research-papers-published/${tab.tab_url_slug}`} className="relative group">
+                        <Link href={`${basePath}faculty/research-papers-published/${tab.tab_url_slug}#research-publication`} className="relative group">
                             <span>{tab.tab_title}</span>
                             <span className={`absolute left-0 -bottom-1 origin-center transition-transform duration-300 w-full h-[0.5px] bg-[#800000] scale-x-0 group-hover:scale-x-100 ${page_name === tab.tab_title ? 'scale-x-100': ''}`}></span>
                         </Link>
