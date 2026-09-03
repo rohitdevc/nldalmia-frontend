6b8a5694a1898e4f14bff027c0a782a23c3a3066
unknown <rohitc129@gmail.com>
2026-09-03 18:20:53 +0530

    ifrc page

---
+++ app/ifrc-2027/page.tsx
@@ -0,0 +1,61 @@
+import { getMetaData, getBanner } from "@/lib/common";
+
+import type { Metadata } from "next";
+import "./style.css"
+import IFRC2027 from "@/components/pages/IFRC2027";
+
+const [
+  meta,
+  banner
+] = await Promise.all([
+  getMetaData("IFRC 2027"),
+  getBanner("Home")
+]);
+
+export const viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: process.env.DEFAULT_THEME_COLOUR },
+    { media: "(prefers-color-scheme: dark)", color: process.env.DEFAULT_THEME_COLOUR },
+  ],
+};
+
+export const revalidate = 300;
+
+const basePath = process.env.NEXT_PUBLIC_DOMAIN_NAME;
+
+const canonical_tag = basePath + meta.canonical_tag;
+
+export const metadata: Metadata = {
+  title: meta.meta_title,
+  description: meta.meta_description,
+  alternates: {
+    canonical: canonical_tag
+  },
+  openGraph: {
+      title: meta.meta_title,
+      description: meta.meta_description,
+      type: "website",
+      url: canonical_tag,
+      siteName: "NL Dalmia",
+      images: [
+        {
+          url: banner.banner_image,
+          width: 1200,
+          height: 630,
+          alt: meta.meta_title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: meta.meta_title,
+      description: meta.meta_description,
+      images: [banner.banner_image],
+    },
+};
+
+export default async function Page() {
+  return (
+    <IFRC2027 />
+  )
+}
